975ad049e807ac4d02d3a50239000100bb3a8f46
jxzhe <wuyenche@gmail.com>
2018-03-27 12:03:57 +0800

    add footer

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,47 @@
 window.onload = function () {
     var arrow = document.querySelector('header i');
     arrow.onclick = function () {
-        document.querySelector('main').scrollIntoView({
+        document.querySelector('footer').scrollIntoView({
             behavior: 'smooth'
         });
     }
 
+    document.querySelector('footer').style.height = String(window.innerHeight * 0.1) + 'px';
+
     var canvas = document.querySelector('canvas');
-    canvas.width = window.innerWidth; 
-    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight * 0.9;
 
     var context = canvas.getContext('2d');
-    var paint;
-    var prevCursor = {};
+    context.fillStyle = '#FFF';
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
-    canvas.addEventListener('mousedown', function(e) {
-        var cursor = getCursor(canvas, e);
-        draw(cursor);
+    var paint = false;
+    var cursor = {},
+        prevCursor = {};
+
+    canvas.addEventListener('mousedown', function (e) {
+        cursor = getCursor(canvas, e);
+        draw();
         paint = true;
         prevCursor.x = cursor.x;
         prevCursor.y = cursor.y;
     });
 
-    canvas.addEventListener('mousemove', function(e) {
+    canvas.addEventListener('mousemove', function (e) {
         if (paint) {
-            var cursor = getCursor(canvas, e);
-            draw(cursor);
+            cursor = getCursor(canvas, e);
+            draw();
             prevCursor.x = cursor.x;
             prevCursor.y = cursor.y;
         }
     });
 
-    canvas.addEventListener('mouseup', function(e) {
+    canvas.addEventListener('mouseup', function (e) {
         paint = false;
     });
 
-    canvas.addEventListener('mouseleave', function(e) {
+    canvas.addEventListener('mouseleave', function (e) {
         paint = false;
     });
 
@@ -47,10 +53,14 @@ window.onload = function () {
         };
     }
 
-    function draw(cursor) {
-        context.strokeStyle = "red";
+    function draw() {
+        context.strokeStyle = document.querySelector('input[type=color]').value;
+        if (document.querySelector('#Eraser').classList.contains('btn-primary')) {
+            context.strokeStyle = '#FFF';
+        }
+        context.lineWidth = document.querySelector('input[type=range]').value;
         context.lineJoin = "round";
-        context.lineWidth = 5;
+        context.lineCap = "round";
         context.beginPath();
         if (paint) {
             context.moveTo(prevCursor.x, prevCursor.y);
@@ -60,5 +70,59 @@ window.onload = function () {
         context.lineTo(cursor.x, cursor.y);
         context.closePath();
         context.stroke();
+    }
+
+    var buttons = document.querySelectorAll('footer>button');
+    for (var i = 0; i < buttons.length; ++i) {
+        buttons[i].addEventListener('click', function () {
+            var others = document.querySelectorAll('footer>button');
+            for (var i = 0; i < others.length; ++i) {
+                others[i].classList.add('btn-secondary');
+                others[i].classList.remove('btn-primary');
+            }
+            this.classList.add('btn-primary');
+            this.classList.remove('btn-secondary');
+        });
+    }
+
+    document.querySelector('input[type=range]').addEventListener('mousemove', function () {
+        document.querySelector('input[type=range]+div').innerHTML = this.value;
+    });
+
+    document.querySelector('input[type=range]').addEventListener('change', function () {
+        document.querySelector('input[type=range]+div').innerHTML = this.value;
+    });
+
+    document.querySelector('#Refresh').addEventListener('click', function () {
+        context.fillStyle = "#FFF";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+    });
+
+    document.querySelector('#Save').addEventListener('click', function () {
+        this.setAttribute('download', 'paint.png');
+        this.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+        this.click();
+    });
+
+    document.querySelector('input[name=imageLoader]').addEventListener('change', handleImage, false);
+
+    function handleImage(e) {
+        var reader = new FileReader();
+        reader.onload = function (event) {
+            var img = new Image();
+            img.onload = function () {
+                var width = img.width;
+                var height = img.height;
+                if (img.width / img.height > canvas.width / canvas.height) {
+                    height *= canvas.width / width;
+                    context.drawImage(img, 0, (canvas.height - height) / 2, canvas.width, height);
+                } else {
+                    width *= canvas.height / height;
+                    context.drawImage(img, (canvas.width - width) / 2, 0, width, canvas.height);
+                }
+            }
+            img.src = event.target.result;
+        }
+        reader.readAsDataURL(e.target.files[0]);
     }
 }
